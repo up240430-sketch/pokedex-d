c11ce3497c15b0af0c9f6a942e0a3e9264bfbe5b
Diego Gonzalez <up240430@alumnos.upa.edu.mx>
2026-03-03 15:34:02 -0600

    add getPokemon fetch

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  useEffect(() => {
+    console.log("Entre en pantalla");
+    getPokemon();
+  }, []);
+
+  const getPokemon = async () => {
+    const url = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  };
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text>Diego</Text>
     </View>
   );
 }
